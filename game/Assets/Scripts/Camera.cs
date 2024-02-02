e2ae6f6c06be790e50c00973d367d966f3bd0def
MattZack15 <97991922+MattZack15@users.noreply.github.com>
2024-02-02 16:46:31 -0500

    CameraMovement

---
+++ game/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform playertransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playertransform.position.x, playertransform.position.y, transform.position.z);
+    }
+}
