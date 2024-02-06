fe9a6962c5209f18ae83a9edf74665b2650489df
MattZack15 <97991922+MattZack15@users.noreply.github.com>
2024-02-05 21:06:57 -0500

    Book Obstacle
    Book is moving and will commit die to you

---
+++ game/Assets/Scripts/BookObstacle.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookObstacle : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+
+    public float moveSpeed = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MoveToPosition1());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().GetHit(DamageType.Spike);
+        }
+    }
+
+    IEnumerator MoveToPosition1()
+    {
+        while (Vector2.Distance(pos1.position, transform.position) > .1f)
+        {
+            Vector2 dir = (pos1.position - transform.position).normalized;
+            transform.position += (Vector3)(dir * moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        StartCoroutine(MoveToPosition2());
+    }
+    IEnumerator MoveToPosition2()
+    {
+        while (Vector2.Distance(pos2.position, transform.position) > .1f)
+        {
+            Vector2 dir = (pos2.position - transform.position).normalized;
+            transform.position += (Vector3)(dir * moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        StartCoroutine(MoveToPosition1());
+    }
+}
