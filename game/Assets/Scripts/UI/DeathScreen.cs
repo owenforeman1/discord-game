61ab1edcb66b160072b1d684b0781b81e0d926d7
MattZack15 <97991922+MattZack15@users.noreply.github.com>
2024-02-12 17:19:40 -0500

    patch

--- game/Assets/Scripts/UI/DeathScreen.cs
+++ game/Assets/Scripts/UI/DeathScreen.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Microsoft.Unity.VisualStudio.Editor;
 
 public class DeathScreen : MonoBehaviour
 {
