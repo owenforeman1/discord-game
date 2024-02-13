7f51dff81a750c799c0f7bfdb6e304e393e21c74
MattZack15 <97991922+MattZack15@users.noreply.github.com>
2024-02-12 21:52:55 -0500

    Timer

---
+++ game/Assets/Scripts/UI/SpeedRunTimer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class SpeedRunTimer : MonoBehaviour
+{
+    public TMP_Text text;
+
+    public float StartTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentTime = Time.time;
+        int minutes = (int)currentTime / 60;
+        string seconds = ((int)(currentTime - (float)minutes * 60)).ToString();
+        if(seconds.Length == 1)
+        {
+            seconds = "0" + seconds;
+        }
+
+
+        string microseconds = currentTime.ToString("0.00");
+        microseconds = microseconds.Substring(microseconds.Length - 2);
+        //string microseconds = Math.Round((float)currentTime - (float)(minutes * 60) - (float)seconds, 2);
+
+
+        text.text = $"{minutes}:{seconds}.{microseconds}";
+    }
+}
